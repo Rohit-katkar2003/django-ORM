492266098f7c655fa6586c73dfeaca70a7387225
Akshada <akshada1kusalkar@gmail.com>
2025-10-08 18:46:47 +0530

    Initial project upload

--- core/scripts/orm_script.py
+++ core/scripts/orm_script.py
@@ -1,5 +1,36 @@
-def run(): 
-    return f'hello this is orm_script' 
+from core.models import Restaurant 
+from django.utils import timezone
+from django.db import connection  ## here it connect to database
 
-if __name__ == '__main__': 
-    run()
+# def run():  ## here we just add the data to it
+
+#     restaurant = Restaurant() 
+#     restaurant.name = "my indian Restaurant" 
+#     restaurant.latitude = 40.9 
+#     restaurant.longitude = 49.9  
+#     restaurant.date  = timezone.now() 
+#     restaurant.restaurant_type = Restaurant.TypeChoices.INDIAN 
+    
+#     restaurant.save() # it is responsible for save the data in the database
+#     print("save the data")  
+
+def run(): 
+    ## to create new data u can also use the  
+    restaurant = Restaurant() 
+    Restaurant.objects.create(
+        name='dominos pizza' , 
+        date = timezone.now() , 
+        restaurant_type = restaurant.TypeChoices.ITALIAN , 
+        latitude = 0.32 , 
+        longitude = 9.23
+    )
+    
+# def run(): 
+#     restaurant = Restaurant.objects.all() ## this function is to retreive the data from database 
+#     # print(restaurant)   
+#     get_1strow = Restaurant.objects.first() 
+#     print("1st Row is := " , get_1strow)
+#     print(connection.queries)
+    
+# if __name__ == '__main__': 
+#     get_all_data()
